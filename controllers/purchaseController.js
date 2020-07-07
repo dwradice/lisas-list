990a0cb1e867d50326f1d0bad1ff5f6d50226779
dwradice <dwradic@gmail.com>
2020-07-06 20:52:06 -0700

    hope this works

--- controllers/purchaseController.js
+++ controllers/purchaseController.js
@@ -60,7 +60,6 @@ exports.checkout = (req, res, next) => {
   }
   if (event.type === 'checkout.session.completed') {
     this.createPurchaseCheckout(event.data.object);
-
-    res.status(200).json({ received: true });
   }
+  res.status(200).json({ received: true });
 };
